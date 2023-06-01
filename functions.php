823ae37d273cb9b4b285d9c6481649971db82d3b
Benjamin Lu <benlumia007@users.noreply.github.com>
2023-06-01 09:14:42 -0700

    initial commit

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Default functions template
+ *
+ * This file is used to bootstrap the theme.
+ *
+ * @package   Succotash
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2023. Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://github.com/benlumia007/luthemes.com
+ */
+
+/** ------------------------------------------------------------------------------------------
+ * Load composer files.
+ * -------------------------------------------------------------------------------------------
+ * Please load the composer files first to ensure that any classes or functions that we may
+ * require are available through autoload.
+ */
+
+if ( file_exists( get_theme_file_path( '/vendor/autoload.php' ) ) ) {
+	require_once get_theme_file_path( '/vendor/autoload.php' );
+}
