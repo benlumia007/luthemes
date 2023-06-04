08f99e8eec33f6539bdd90466fb4217b7089b20e
Benjamin Lu <benlumia007@users.noreply.github.com>
2023-06-04 14:37:13 -0700

    commit everything

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -7,7 +7,41 @@
   \*****************************/
 /***/ (() => {
 
+/**
+ * Primary front-end script.
+ *
+ * Primary JavaScript file. Any includes or anything imported should be filtered through this file
+ * and eventually saved back into the `/assets/js/app.js` file.
+ *
+ * @package   Succotash
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2014-2022 Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://github.com/benlumia007/luthemes.com
+ */
 
+/**
+ * A simple immediately-invoked function expression to kick-start
+ * things and encapsulate our code.
+ *
+ * @since  1.0.0
+ * @access public
+ * @return void
+ */
+(function ($) {
+  $('.data-item').click(function () {
+    var value = $(this).attr('data-filter');
+    if (value == 'all') {
+      $('.portfolio-item').show();
+    } else {
+      $('.portfolio-item').not('.' + value).hide();
+      $('.portfolio-item').filter('.' + value).show();
+    }
+  });
+  $('.data-item').click(function () {
+    $(this).addClass('active').siblings().removeClass('active');
+  });
+})(jQuery);
 
 /***/ }),
 
